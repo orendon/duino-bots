778b834ff53c1c5f7959d0649168b1b2546f5dc1
Oscar Rendon <oarendon@gmail.com>
2013-08-03 18:01:46 -0500

    Add http server and basic rgbled view.

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path');
+
+var app = express(),
+    server = http.createServer(app)
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/rgb_led', function(req, res){
+  res.render('rgb_led');
+});
+
+server.listen(3000);
